f3d62ca40d059de1d54f936cd939e5e0adcaf04b
dangnk <dangnk@vnpay.vn>
2024-01-24 11:20:07 +0700

    dangnk/mysqlcourse:mysql-1

--- categories/models.py
+++ categories/models.py
@@ -21,16 +21,15 @@ class SubCategory(models.Model):
 
 class Blogs(models.Model):
     name = models.CharField(max_length=500, null=True)
-    images = models.CharField(null=True, blank=True)
-    linkvideo = models.CharField(null=True, blank=True)
-    linkredirect = models.CharField(null=True, blank=True)
-    content = models.TextField(null=True, blank=True)  # Trường TextField để lưu nội dung blog
+    images = models.CharField(max_length=255, null=True, blank=True)
+    linkvideo = models.CharField(max_length=255, null=True, blank=True)
+    linkredirect = models.CharField(max_length=255, null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     subcategory = models.ForeignKey(
         SubCategory, on_delete=models.CASCADE, null=True)
-    
 
     def __str__(self):
         return self.name
