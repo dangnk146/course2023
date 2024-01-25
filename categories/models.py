46722917154f9a8a3a333d6d93a71908f3ad301a
dangnk <dangnk@vnpay.vn>
2024-01-25 11:58:42 +0700

    dangnk/test:mysql-2

--- categories/models.py
+++ categories/models.py
@@ -21,15 +21,14 @@ class SubCategory(models.Model):
 
 class Blogs(models.Model):
     name = models.CharField(max_length=500, null=True)
-    images = models.CharField(max_length=255, null=True, blank=True)
-    linkvideo = models.CharField(max_length=255, null=True, blank=True)
-    linkredirect = models.CharField(max_length=255, null=True, blank=True)
+    images = models.URLField(max_length=255, null=True, blank=True)
+    linkvideo = models.URLField(max_length=255, null=True, blank=True)
+    linkredirect = models.URLField(max_length=255, null=True, blank=True)
     content = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    subcategory = models.ForeignKey(
-        'SubCategory', on_delete=models.CASCADE, null=True)
+    subcategory = models.ForeignKey('SubCategory', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
